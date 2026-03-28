8191fc2e9f55c96da9b5a9e6a59d6c03beb7f754
denislirette <denislirette@gmail.com>
2026-03-28 11:17:12 -0400

    Fix VitePress base path for GitHub Pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  base: '/baseballscorecard/',
   title: 'BaseballScorecard.org',
   description: 'System documentation for BaseballScorecard.org',
 
